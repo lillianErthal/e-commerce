2996e6a3e1ed2a088ba647d3ad1e80d724c08a6a
lillianErthal <lillian_erthal@estudante.sesisenai.org.br>
2024-06-27 12:28:00 -0300

    atualiza

--- mercado/__init__.py
+++ mercado/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
 app.config["SECRET_KEY"] = '4ce88ce596e2691188718bdd'
 db.init_app(app)
+bcrypt = Bcrypt(app)
 
 from mercado import routes
--- mercado/forms.py
+++ mercado/forms.py
@@ -1,8 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from mercado.models import User
 
 class CadastroForm(FlaskForm):
+    def validate_username(self, check_user):
+        user = User.query.filter_by(usuario=check_user.data).first()
+        if user:
+            raise ValidationError("Usuário já existe! Cadastre outro usuário.") 
+        
+    def validate_email(self, check_email):
+        email = User.query.filter_by(email=check_email.data).first()
+        if email:
+            raise ValidationError("E-mail já existe! Cadastre outro e-mail.") 
+        
+    def validate_senha(self, check_senha):
+        senha = User.query.filter_by(senha=check_senha.data).first()
+        if senha:
+            raise ValidationError("Senha já existe! Cadastre outro senha.") 
+        
     usuario = StringField(label= 'Username: ', validators=[Length(min=2, max=30), DataRequired()])
     email = StringField(label= 'E-mail: ', validators=[Email(), DataRequired()])
     senha1 = PasswordField(label= 'Senha: ', validators=[Length(min=6), DataRequired()])

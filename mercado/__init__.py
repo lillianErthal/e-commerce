444cbe66c07249fe2a13a0794400e30f4a7bffb9
lillianErthal <lillian_erthal@estudante.sesisenai.org.br>
2024-06-27 11:25:52 -0300

    a

---
+++ mercado/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
+app.config["SECRET_KEY"] = '4ce88ce596e2691188718bdd'
+db.init_app(app)
+
+from mercado import routes
